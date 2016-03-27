8884284e96c2b35f9db2118713216edcce7a11e6
XIN HE <carolxinhe@192-168-1-3.tpgi.com.au>
2016-03-27 16:52:05 +1100

    add form

--- public/app/controllers/agentRegistrationCtrl.js
+++ public/app/controllers/agentRegistrationCtrl.js
@@ -3,4 +3,16 @@
 
 function RegistrationForm ($scope) {
 	$scope.isRegisted = 'zzzzzz hello zzzzzz';
+
+	var agentList = [];
+
+	$scope.addAgents = function() {
+		console.log('add ' + $scope.agentToAdd + ' to the list');
+		agentList.push($scope.agentToAdd);
+		$scope.agentToAdd = null;
+	}
+
+	$scope.listAgents = function() {
+		return agentList;
+	}
 }
